d50b61a217409130da43d12ba46613c66cff7510
Jan Stamer <914776+remast@users.noreply.github.com>
2023-03-20 09:19:06 +0100

    Added gotests.

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -24,7 +24,7 @@ go install -v github.com/go-delve/delve/cmd/dlv@latest
 echo "Getting dependencies for the vscode-go plugin "
 # via: https://github.com/microsoft/vscode-go/blob/master/.travis.yml
 go install -v github.com/acroca/go-symbols@latest
-# go install -v github.com/cweill/gotests/...
+go install -v github.com/cweill/gotests/...@latest
 go install -v github.com/davidrjenni/reftools/cmd/fillstruct@latest
 go install -v github.com/haya14busa/goplay/cmd/goplay@latest
 go install -v github.com/mdempsky/gocode@latest
