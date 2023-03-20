ecd2a33390b34e318f471856a2af5d4392f6390f
Jan Stamer <914776+remast@users.noreply.github.com>
2023-03-20 09:07:51 +0100

    Test

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,5 +1,5 @@
 # install curl, git, ...
-apt-get update
+#apt-get update
 apt-get install -y curl git jq
 
 useradd -m user
@@ -19,21 +19,21 @@ echo "Go version ${INSTALLED_GO_VERSION} is installed"
 
 # install gopls, dlv, hey
 echo "Getting development tools"
-go get -u golang.org/x/tools/gopls
-go get -u github.com/go-delve/delve/cmd/dlv
+go install -u golang.org/x/tools/gopls@latest
+#go install -u github.com/go-delve/delve/cmd/dlv@latest
 
 # vscode-go dependencies 
 echo "Getting dependencies for the vscode-go plugin "
 # via: https://github.com/microsoft/vscode-go/blob/master/.travis.yml
-go get -u -v github.com/acroca/go-symbols
-go get -u -v github.com/cweill/gotests/...
-go get -u -v github.com/davidrjenni/reftools/cmd/fillstruct
-go get -u -v github.com/haya14busa/goplay/cmd/goplay
-go get -u -v github.com/mdempsky/gocode
-go get -u -v github.com/ramya-rao-a/go-outline
-go get -u -v github.com/rogpeppe/godef
-go get -u -v github.com/sqs/goreturns
-go get -u -v github.com/uudashr/gopkgs/v2/cmd/gopkgs
-go get -u -v github.com/zmb3/gogetdoc
-go get -u -v golang.org/x/lint/golint
-go get -u -v golang.org/x/tools/cmd/gorename
+#go install -v github.com/acroca/go-symbols@latest
+# go install -v github.com/cweill/gotests/...
+#go install -v github.com/davidrjenni/reftools/cmd/fillstruct@latest
+#go install -v github.com/haya14busa/goplay/cmd/goplay@latest
+#go install -v github.com/mdempsky/gocode@latest
+#go install -v github.com/ramya-rao-a/go-outline@latest
+#go install -v github.com/rogpeppe/godef@latest
+#go install -v github.com/sqs/goreturns@latest
+#go install -v github.com/uudashr/gopkgs/v2/cmd/gopkgs@latest
+#go install -v github.com/zmb3/gogetdoc@latest
+#go install -v golang.org/x/lint/golint@latest
+#go install -v golang.org/x/tools/cmd/gorename@latest
